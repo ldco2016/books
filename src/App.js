2a0d8c6ef3e451a33ba7e2157ec2730e873b013f
ldco2016 <renaissance.scholar2012@gmail.com>
2023-01-24 12:08:53 -0600

    Added useCallback hook to fix stale reference variable bug

--- src/App.js
+++ src/App.js
@@ -1,70 +1,20 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
+import BooksContext from './context/books';
 
 function App() {
-  const [books, setBooks] = useState([]);
-
-  const fetchBooks = async () => {
-    const response = await axios.get(
-      'https://my-json-server.typicode.com/ldco2016/json-book-server'
-    );
-
-    setBooks(response.data);
-  };
+  const { fetchBooks } = useContext(BooksContext);
 
   useEffect(() => {
     fetchBooks();
-  }, []);
-
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(
-      `https://my-json-server.typicode.com/ldco2016/json-book-server/${id}`
-    );
-
-    console.log(response);
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data };
-      }
-
-      return book;
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const deleteBookById = async (id) => {
-    await axios.delete(
-      `https://my-json-server.typicode.com/ldco2016/json-book-server/${id}`
-    );
-
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const createBook = async (title) => {
-    const response = await axios.post(
-      'https://my-json-server.typicode.com/ldco2016/json-book-server',
-      {
-        title,
-      }
-    );
-
-    const updatedBooks = [...books, response.data];
-    setBooks(updatedBooks);
-  };
+  }, [fetchBooks]);
 
   return (
     <div className='app'>
       <h1>Reading List</h1>
-      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
-      <BookCreate onCreate={createBook} />
+      <BookList />
+      <BookCreate />
     </div>
   );
 }
